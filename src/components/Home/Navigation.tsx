12559258f48cc1f3caebd5b7e7a68f481fc899a9
Laam Wong <laamwong3@gmail.com>
2024-04-26 18:08:00 +0800

    add navbar

--- src/components/Home/Navigation.tsx
+++ src/components/Home/Navigation.tsx
@@ -1,7 +1,95 @@
+"use client";
 import React from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  NavbarMenuToggle,
+  NavbarMenu,
+  Link,
+  Button,
+} from "@nextui-org/react";
+import Image from "next/image";
+import web3in from "@/assets/icons/web3in.webp";
 
-const Navigation = () => {
-  return <div>lorem100</div>;
-};
+export default function Navigation() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
-export default Navigation;
+  return (
+    <Navbar onMenuOpenChange={setIsMenuOpen} maxWidth="full">
+      <NavbarContent>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="md:hidden"
+        />
+        <NavbarBrand>
+          <Image src={web3in} alt="web3in logo" height={50} />
+        </NavbarBrand>
+      </NavbarContent>
+      <NavbarContent className="hidden gap-4 md:flex" justify="center">
+        {menuItems.map((item) => (
+          <NavbarItem key={item.id}>
+            <Link
+              href={item.link}
+              color="foreground"
+              className="text-xl"
+              underline="hover"
+            >
+              {item.label}
+            </Link>
+          </NavbarItem>
+        ))}
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem>
+          <Button as={Link} color="primary" href="#" variant="ghost">
+            Get Started
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarMenu>
+        {menuItems.map((item) => (
+          <NavbarItem key={item.id}>
+            <Link
+              href={item.link}
+              color="foreground"
+              className="text-2xl"
+              underline="hover"
+            >
+              {item.label}
+            </Link>
+          </NavbarItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
+  );
+}
+
+const menuItems = [
+  {
+    id: "services",
+    label: "Services",
+    link: "#",
+  },
+  {
+    id: "designs",
+    label: "Designs",
+    link: "#",
+  },
+  {
+    id: "pricing",
+    label: "Pricing",
+    link: "#",
+  },
+  {
+    id: "faq",
+    label: "FAQ",
+    link: "#",
+  },
+  {
+    id: "contact",
+    label: "Contact Us",
+    link: "#",
+  },
+];
