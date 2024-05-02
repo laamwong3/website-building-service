d3f1e9d6ce9111b2dc7f415833a4562de60ac7e3
Laam Wong <laamwong@Laams-MacBook-Air.local>
2024-05-02 10:21:57 +0800

    add footer component

--- src/components/Home/Contact.tsx
+++ src/components/Home/Contact.tsx
@@ -1,5 +1,6 @@
 import { type LucideProps, Mail, MapPin } from "lucide-react";
 import * as React from "react";
+import SectionSplit from "./SectionSplit";
 
 interface ContactInfoProps {
   Icon: React.ForwardRefExoticComponent<
@@ -37,49 +38,52 @@ const ContactInfo: React.FC<ContactInfoProps> = ({
 
 const Contact = () => {
   return (
-    <section className="flex flex-col items-center pb-16 max-md:flex-wrap md:px-16 md:py-16">
-      <div className="w-full">
-        <div className="flex gap-5 max-md:flex-col max-md:gap-0">
-          <div className="flex w-[59%] flex-col max-md:ml-0 max-md:w-full">
-            <div className="flex flex-col leading-[150%] max-md:mt-10 max-md:max-w-full">
-              <h1 className="text-2xl font-bold leading-[57.6px] max-md:max-w-full md:text-4xl">
-                Get in Touch
-              </h1>
-              <p className="mt-6 text-lg text-neutral-400 max-md:max-w-full">
-                We&apos;re here to answer any questions you may have. Contact us
-                today!
-              </p>
+    <>
+      <section className="flex flex-col items-center pb-16 max-md:flex-wrap md:px-16 md:py-16">
+        <div className="w-full">
+          <div className="flex gap-5 max-md:flex-col max-md:gap-0">
+            <div className="flex w-[59%] flex-col max-md:ml-0 max-md:w-full">
+              <div className="flex flex-col leading-[150%] max-md:mt-10 max-md:max-w-full">
+                <h1 className="text-2xl font-bold leading-[57.6px] max-md:max-w-full md:text-4xl">
+                  Get in Touch
+                </h1>
+                <p className="mt-6 text-lg text-neutral-400 max-md:max-w-full">
+                  We&apos;re here to answer any questions you may have. Contact
+                  us today!
+                </p>
+              </div>
             </div>
-          </div>
-          <div className="ml-5 flex w-[41%] flex-col max-md:ml-0 max-md:w-full">
-            <div className="mt-4 flex grow flex-col max-md:max-w-full md:mt-0">
-              {contactData.map((contact, index) => (
-                <ContactInfo key={index} {...contact} />
-              ))}
+            <div className="ml-5 flex w-[41%] flex-col max-md:ml-0 max-md:w-full">
+              <div className="mt-4 flex grow flex-col max-md:max-w-full md:mt-0">
+                {contactData.map((contact, index) => (
+                  <ContactInfo key={index} {...contact} />
+                ))}
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      {/* <Image
+        {/* <Image
         loading="lazy"
         src=""
         alt="Contact us banner"
         className="mt-20 aspect-[1.89] w-full max-md:mt-10 max-md:max-w-full"
       /> */}
-      <div className="max-md:max-w-ful mt-20 aspect-[1.89] w-full overflow-hidden rounded-xl bg-background max-md:mt-10">
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3383.0745203874108!2d115.84651127522858!3d-32.013095025350324!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2a32a34daa204c8d%3A0x57ee78233ca9c99f!2s3b%2F907%20Canning%20Hwy%2C%20Mount%20Pleasant%20WA%206153!5e0!3m2!1sen!2sau!4v1714602148547!5m2!1sen!2sau"
-          width="100%"
-          height="100%"
-          style={{
-            border: 0,
-            filter: "grayscale(10%) invert(100%)",
-          }}
-          allowFullScreen
-          referrerPolicy="no-referrer-when-downgrade"
-        ></iframe>
-      </div>
-    </section>
+        <div className="max-md:max-w-ful mt-20 aspect-[1.89] w-full overflow-hidden rounded-xl bg-background max-md:mt-10">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3383.0745203874108!2d115.84651127522858!3d-32.013095025350324!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2a32a34daa204c8d%3A0x57ee78233ca9c99f!2s3b%2F907%20Canning%20Hwy%2C%20Mount%20Pleasant%20WA%206153!5e0!3m2!1sen!2sau!4v1714602148547!5m2!1sen!2sau"
+            width="100%"
+            height="100%"
+            style={{
+              border: 0,
+              filter: "grayscale(10%) invert(100%)",
+            }}
+            allowFullScreen
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </div>
+      </section>
+      <SectionSplit />
+    </>
   );
 };
 
--- src/components/Home/Footer.tsx
+++ src/components/Home/Footer.tsx
@@ -1,7 +1,63 @@
-import React from "react";
+import * as React from "react";
+import web3in from "@/assets/icons/web3in.webp";
+import Image from "next/image";
 
-const Footer = () => {
-  return <div>Footer</div>;
-};
+// interface SocialIconProps {
+//   src: string;
+//   alt: string;
+// }
+
+// const SocialIcon: React.FC<SocialIconProps> = ({ src, alt }) => (
+//   <img
+//     loading="lazy"
+//     src={src}
+//     alt={alt}
+//     className="aspect-square w-6 shrink-0"
+//   />
+// );
+
+function Footer() {
+  return (
+    <footer className="flex flex-col items-center pb-16 max-md:flex-wrap md:px-16">
+      <div className="flex w-full flex-col items-center justify-center gap-5 max-md:max-w-full max-md:flex-wrap">
+        <Image src={web3in} alt="web3in logo" height={50} />
+        <div className="my-auto flex gap-2 max-md:flex-wrap">
+          <div className="flex flex-1 gap-3">
+            {/* {socialIcons.map(({ src, alt }, index) => (
+              <SocialIcon key={index} src={src} alt={alt} />
+            ))} */}
+          </div>
+        </div>
+      </div>
+      <div className="mt-8 h-px w-full shrink-0 border-b-[0.5px] border-white max-md:mt-10" />
+      <div className="mt-8 flex justify-between gap-5 self-center text-sm leading-5 max-md:flex-wrap">
+        <div>2024 Web3in. All right reserved.</div>
+      </div>
+    </footer>
+  );
+}
 
 export default Footer;
+
+// const socialIcons = [
+//   {
+//     src: "https://cdn.builder.io/api/v1/image/assets/TEMP/7cb14ad2a93b6b3a5f6779774b00ea9979e4593abdd26f850601973b3da4ab21?apiKey=2ed0e0cd3d194791a0d86a908e2022b7&",
+//     alt: "Social Icon 1",
+//   },
+//   {
+//     src: "https://cdn.builder.io/api/v1/image/assets/TEMP/0ea003e49fd93659e1550be5605e2cfa00850629e05d9c9a3ab4d9a5231c7d49?apiKey=2ed0e0cd3d194791a0d86a908e2022b7&",
+//     alt: "Social Icon 2",
+//   },
+//   {
+//     src: "https://cdn.builder.io/api/v1/image/assets/TEMP/c526b5f36ed774814623889a298073f6b03a54ab7a1d44d33a212340c19561f9?apiKey=2ed0e0cd3d194791a0d86a908e2022b7&",
+//     alt: "Social Icon 3",
+//   },
+//   {
+//     src: "https://cdn.builder.io/api/v1/image/assets/TEMP/c7dc4208c3c955b42542a250641d3ad0c2aa7f782e1cc8e26b90e21427e49696?apiKey=2ed0e0cd3d194791a0d86a908e2022b7&",
+//     alt: "Social Icon 4",
+//   },
+//   {
+//     src: "https://cdn.builder.io/api/v1/image/assets/TEMP/0dc00635b373d3e5de68441d9146c25e003ce1b4d547c3f3078043913a3b2af5?apiKey=2ed0e0cd3d194791a0d86a908e2022b7&",
+//     alt: "Social Icon 5",
+//   },
+// ];
