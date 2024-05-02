391f710bc5567ee279bb76799127c4ac2e2331f1
Laam Wong <laamwong3@gmail.com>
2024-05-02 13:05:27 +0800

    add animation on hero page

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -1,17 +1,34 @@
+"use client";
 import * as React from "react";
 import { Button } from "@nextui-org/button";
+import { motion } from "framer-motion";
 
 const Hero = () => {
   return (
     <section className="flex min-h-[calc(100vh-64px)] flex-col items-center justify-center overflow-hidden">
-      <h1 className="w-[868px] text-center text-6xl font-bold leading-[67px] max-md:mt-10 max-md:max-w-full max-md:text-4xl max-md:leading-[54px]">
+      <motion.h1
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+        className="w-[868px] bg-gradient-to-r from-purple-500 to-purple-900 bg-clip-text text-center text-6xl font-bold leading-[67px] text-transparent max-md:mt-10 max-md:max-w-full max-md:text-4xl max-md:leading-[54px]"
+      >
         Own Your Beautiful Websites with Ease
-      </h1>
-      <p className="mt-6 w-[768px] text-center text-lg leading-7 text-neutral-400 max-md:max-w-full">
+      </motion.h1>
+      <motion.p
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1, delay: 0.5 }}
+        className="mt-6 w-[768px] text-center text-lg leading-7 text-neutral-400 max-md:max-w-full"
+      >
         Our service provides user-friendly, customizable website building for
         unique, standout sites that reflect your brand and vision
-      </p>
-      <div className="mt-6 flex items-start gap-4 whitespace-nowrap pt-4 text-base leading-6">
+      </motion.p>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1, delay: 1.5 }}
+        className="mt-6 flex items-start gap-4 whitespace-nowrap pt-4 text-base leading-6"
+      >
         <Button
           className="justify-center border border-solid px-6 py-3 max-md:px-5"
           variant="solid"
@@ -25,7 +42,7 @@ const Hero = () => {
         >
           Learn More
         </Button>
-      </div>
+      </motion.div>
     </section>
   );
 };
